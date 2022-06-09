25f5f32a28b47e2e8f5051987196f5bc4b751653
Maribel <lwang79@zoho.com>
2022-06-09 11:43:47 -0700

    starting code of the challenge

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// commented out works in version >18, but not in 17
+// const root = ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
